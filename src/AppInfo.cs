53108360ec2fc1ba628e37397095527423f262f4
sl2365 <76538604+sl2365@users.noreply.github.com>
2026-04-21 18:44:31 +0100

    Add files via upload

---
+++ src/AppInfo.cs
@@ -0,0 +1,8 @@
+namespace ElementalTracker
+{
+    public static class AppInfo
+    {
+        public const string AppName = "Elemental Tracker";
+        public const string ShortName = "ET";  // replaces AppInfo.ShortName
+    }
+}
